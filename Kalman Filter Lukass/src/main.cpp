7c15368b00c49490a9a5adc60c00bb66a8a04a83
lukass16 <lukasskellijs@gmail.com>
2022-02-09 19:11:13 +0200

    init Kalman Filter Lukass

---
+++ Kalman Filter Lukass/src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <SimpleKalmanFilter.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Kalman Filter tests");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
