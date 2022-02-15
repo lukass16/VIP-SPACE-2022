0b345080a31af4649c0461a93df3e4e499ac6895
lukass16 <lukasskellijs@gmail.com>
2022-02-15 14:38:35 +0200

    Update structure and add preliminary example

--- Kalman Filter Lukass/src/main.cpp
+++ Kalman Filter Lukass/src/main.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
-#include <SimpleKalmanFilter.h>
 #include <Kalman.h>
+
+float p_baro = 0;
+float v_baro = 5; //*testing with constant velocity model (a=0)
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Kalman Filter tests");
+  kalman::predict();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  kalman::predict();
+  delay(500);
+  kalman::printKalmanState();
+  kalman::printPostionUncertainty();
+  kalman::updateBaro(p_baro); //this doesn't necessarily have to be done right before prediction (extrapolation) but can be just done every time a new reading is available
+
+  //change p_baro for test
+  p_baro = 0.5 * v_baro;
 }
