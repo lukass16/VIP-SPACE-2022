fe812cf37a6897ba785f33d42d286270a3e866aa
lukass16 <lukasskellijs@gmail.com>
2022-03-23 18:58:50 +0200

    make mpu6050 wrapper

---
+++ Helltec MPU6050/include/accelerometer.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+Adafruit_MPU6050 mpu;
+sensors_event_t a, g, temp;
+
+//todo: add personal variables like acc_x etc. and make getData function
+
+namespace accelerometer
+{
+    void setup()
+    {
+        if (!mpu.begin(0x68))
+        {
+            Serial.println("Failed to find MPU6050 chip");
+            while (1)
+            {
+                delay(10);
+            }
+        }
+        Serial.println("MPU6050 Found!");
+
+        //set ranges
+        mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+        mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+        mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+    }
+
+    void printAll()
+    {
+        mpu.getEvent(&a, &g, &temp);
+
+        /* Print out the values */
+        Serial.print("Acceleration X: ");
+        Serial.print(a.acceleration.x);
+        Serial.print(", Y: ");
+        Serial.print(a.acceleration.y);
+        Serial.print(", Z: ");
+        Serial.print(a.acceleration.z);
+        Serial.println(" m/s^2");
+
+        Serial.print("Rotation X: ");
+        Serial.print(g.gyro.x);
+        Serial.print(", Y: ");
+        Serial.print(g.gyro.y);
+        Serial.print(", Z: ");
+        Serial.print(g.gyro.z);
+        Serial.println(" rad/s");
+
+        Serial.print("Temperature: ");
+        Serial.print(temp.temperature);
+        Serial.println(" °C");
+        Serial.print("\n");
+    }
+
+}
--- Helltec MPU6050/src/main.cpp
+++ Helltec MPU6050/src/main.cpp
@@ -1,60 +1,18 @@
 #include <Arduino.h>
-#include "Wire.h"
-#include <MPU6050_light.h>
+#include "accelerometer.h"
 
-MPU6050 mpu(Wire);
 
-long timer = 0;
 
-void setup()
+void setup(void)
 {
 	Serial.begin(115200);
-	Wire.begin();
-
-	byte status = mpu.begin();
-	Serial.print(F("MPU6050 status: "));
-	Serial.println(status);
-	while (status != 0)
-	{
-	} // stop everything if could not connect to MPU6050
-
-	Serial.println(F("Calculating offsets, do not move MPU6050"));
-	delay(1000);
-	mpu.calcOffsets(true, true); // gyro and accelero
-	Serial.println("Done!\n");
+	Serial.println("Adafruit MPU6050 test!");
+	accelerometer::setup();
+	
 }
 
 void loop()
 {
-	mpu.update();
-
-		Serial.print(F("TEMPERATURE: "));
-		Serial.println(mpu.getTemp());
-		Serial.print(F("ACCELERO  X: "));
-		Serial.print(mpu.getAccX());
-		Serial.print("\tY: ");
-		Serial.print(mpu.getAccY());
-		Serial.print("\tZ: ");
-		Serial.println(mpu.getAccZ());
-
-		Serial.print(F("GYRO      X: "));
-		Serial.print(mpu.getGyroX());
-		Serial.print("\tY: ");
-		Serial.print(mpu.getGyroY());
-		Serial.print("\tZ: ");
-		Serial.println(mpu.getGyroZ());
-
-		Serial.print(F("ACC ANGLE X: "));
-		Serial.print(mpu.getAccAngleX());
-		Serial.print("\tY: ");
-		Serial.println(mpu.getAccAngleY());
-
-		Serial.print(F("ANGLE     X: "));
-		Serial.print(mpu.getAngleX());
-		Serial.print("\tY: ");
-		Serial.print(mpu.getAngleY());
-		Serial.print("\tZ: ");
-		Serial.println(mpu.getAngleZ());
-		Serial.println(F("=====================================================\n"));
-
+	accelerometer::printAll();
+	delay(300);
 }
