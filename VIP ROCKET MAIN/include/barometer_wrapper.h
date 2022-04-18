00b2e8d5c009f7793d58c81cfa38411e46a5a8e6
lukass16 <lukasskellijs@gmail.com>
2022-04-18 18:45:01 +0300

    test barometer
    NOTE: There is currently an error when using this Adafruit BMP280 library - this can be fixed by removing the const attributes from the used variables of two functions

--- VIP ROCKET MAIN/include/barometer_wrapper.h
+++ VIP ROCKET MAIN/include/barometer_wrapper.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <Adafruit_BMP280.h>
+#include "sensor_data.h"
 
 #define AVIATION_PRESSURE 1013.5
 
+//! NOTE: There is currently an error when suing this Adafruit library - this can be fixed by removing the const attributes from the used variables of two functions
 
 namespace barometer
 {
@@ -12,7 +14,10 @@ namespace barometer
     // defining necessary variables for vertical velocity calculation
     float h_now = 0, h_prev = 0, t_now = 0, t_prev = 0, dh = 0, dt = 0, vert_velocity = 0, vert_velocity_prev = 0;
 
-    float getVertVelocity();
+    float getVertVelocity() //todo make this
+    {
+        return 42.0;
+    }
 
     void setup(bool read_sea_level = 0)
     {
@@ -51,13 +56,23 @@ namespace barometer
         Serial.println(" *C");
 
         Serial.print("Pressure = ");
-        Serial.print(bmp.readPressure()/100); //displaying the Pressure in hPa, you can change the unit
+        Serial.print(bmp.readPressure() / 100); // displaying the Pressure in hPa, you can change the unit
         Serial.println(" hPa");
 
         Serial.print("Approx altitude = ");
         Serial.print(bmp.readAltitude(sea_level_sample));
         Serial.println(" m");
         Serial.println();
+    }
+
+    sens_data::BarometerData getBarometerState() //*outputs sensor readings from wrapper to be used in the data object
+    {
+        sens_data::BarometerData bd;
+        bd.temperature = bmp.readTemperature();
+        bd.pressure = bmp.readPressure() / 100;
+        bd.altitude = bmp.readAltitude(sea_level_sample);
+        bd.vert_velocity = getVertVelocity();
+        return bd;
     }
 
 }
--- VIP ROCKET MAIN/include/states/preperation_state.cpp
+++ VIP ROCKET MAIN/include/states/preperation_state.cpp
@@ -8,60 +8,69 @@
 #include "wifiserver_wrapper.h"
 #include "buzzer.h"
 #include "gps_wrapper.h"
+#include "barometer_wrapper.h"
 #include "EEPROM.h"
 
-class PreperationState: public State {
-    public: 
+class PreperationState : public State
+{
+public:
+    float it = 0;
 
-        float it = 0;
+    void start() override
+    {
 
-        void start () override {
-            
-            Serial.println("PREP STATE");
+        Serial.println("PREP STATE");
 
-            //*Buzzer test/signal start
-            buzzer::setup();
-            buzzer::signalStart();
-            delay(500);
-            buzzer::buzzEnd();
-            
-            //oled::setup(); //*possibly conflicting with GPS
+        //*Buzzer test/signal start
+        buzzer::setup();
+        buzzer::signalStart();
+        delay(500);
+        buzzer::buzzEnd();
 
-            //*Sensor setups
-            gps::setup();
+        // oled::setup(); //*possibly conflicting with GPS
 
-            comms::setup(433E6);
-  
-            /*  wifiserver::setup(); //*testing wifi in prep loop 
-            while(true) 
-            {
-                it++;
-                sens_data::MagenetometerData md;
-                md.x = it;
-                s_data.setMagnetometerData(md); //setting data in sensor data object
-                wifiserver::setData(); //making the wifi server retrieve/update the data
-                wifiserver::handleClient();
-                delay(100);
-            }*/
+        //*Sensor setups
+        Wire.begin(21, 22); // initialize correct i2c lines
+        gps::setup();
+        barometer::setup();
 
-            delay(2000);
+        comms::setup(433E6);
 
-            //*Sensor reading test loop
-            while(true)
-            {
-                //*gps
-                gps::readGps(); //reads in values from gps
-                sens_data::GpsData gd = gps::getGpsState(); //retrieve values from wrapper to be put in data object
-                s_data.setGpsData(gd);
+        /*  wifiserver::setup(); //*testing wifi in prep loop
+        while(true)
+        {
+            it++;
+            sens_data::MagenetometerData md;
+            md.x = it;
+            s_data.setMagnetometerData(md); //setting data in sensor data object
+            wifiserver::setData(); //making the wifi server retrieve/update the data
+            wifiserver::handleClient();
+            delay(100);
+        }*/
 
-                delay(1000);
-            }
+        delay(2000);
 
-            this->_context->RequestNextPhase();
-            this->_context->Start();
+        //*Sensor reading test loop
+        while (true)
+        {
+            //*gps
+            gps::readGps();                             // reads in values from gps
+            sens_data::GpsData gd = gps::getGpsState(); // retrieve values from wrapper to be put in data object
+            s_data.setGpsData(gd);
+
+            //*barometer
+            sens_data::BarometerData bd = barometer::getBarometerState(); //reads and retrieves values from wrapper to be put in data object
+            s_data.setBarometerData(bd);
+
+            delay(1000);
         }
 
-        void HandleNextPhase() override {
-            this->_context->TransitionTo(new DrogueState);
-        }
+        this->_context->RequestNextPhase();
+        this->_context->Start();
+    }
+
+    void HandleNextPhase() override
+    {
+        this->_context->TransitionTo(new DrogueState);
+    }
 };
