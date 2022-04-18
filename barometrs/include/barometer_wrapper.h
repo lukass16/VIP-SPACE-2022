8d9be215930a8b115aca7eac11c250383abb0843
lukass16 <lukasskellijs@gmail.com>
2022-04-18 18:26:20 +0300

    fix barometer testing library error

--- barometrs/include/barometer_wrapper.h
+++ barometrs/include/barometer_wrapper.h
@@ -1,73 +1,61 @@
 #pragma once
 #include <Adafruit_BMP280.h>
 
+#define AVIATION_PRESSURE 1013.5
+
 Adafruit_BMP280 bmp; // I2C Interface
 
-namespace barometer {
-    const float AVIATION_PRESSURE = 1013.5;
-    float sea_level_read = 0;
-    //defining necessary variables for vertical velocity calculation
+namespace barometer
+{
+
+    float sea_level_sample = 0;
+    // defining necessary variables for vertical velocity calculation
     float h_now = 0, h_prev = 0, t_now = 0, t_prev = 0, dh = 0, dt = 0, vert_velocity = 0, vert_velocity_prev = 0;
 
     float getVertVelocity();
 
-    void setup(bool read_sea_level = 0) 
+    void setup(bool read_sea_level = 0)
     {
-        if (!bmp.begin(0x76)) {
+        if (!bmp.begin(0x76))
+        {
             Serial.println("Could not find a valid BMP280 sensor, check wiring!");
             while (1);
         }
 
         /* Default settings from datasheet. */
         bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                    Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-        if (read_sea_level) {
-            for(int i=0; i<5; i++){
-                sea_level_read+=(bmp.readPressure()/100);
+                        Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                        Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                        Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                        Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+        //*if needed start sampling sea level pressure
+        if (read_sea_level)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                sea_level_sample += (bmp.readPressure() / 100);
             }
-            sea_level_read = sea_level_read / 5;
+            sea_level_sample = sea_level_sample / 5;
         }
         else
         {
-            sea_level_read = AVIATION_PRESSURE;
+            sea_level_sample = AVIATION_PRESSURE;
         }
-        Serial.println("Barometer ready! Sea level pressure: " + String(sea_level_read));
+        Serial.println("Barometer ready! Sea level pressure set as: " + String(sea_level_sample));
     }
 
-    float getVertVelocity()
+    void readSensor()
     {
-        static int counter = 0;
-        counter++;
-        if(counter % 3 == 0) //every 3rd call
-        {
-            h_now = bmp.readAltitude(sea_level_read); //in m
-            t_now = millis() / 1000.0; //in ms
-            dt = t_now - t_prev;
-            dh = h_now - h_prev;
-            vert_velocity = dh/dt;
-            //reverting values
-            h_prev = h_now;
-            t_prev = t_now;
-            vert_velocity_prev = vert_velocity;
-            return vert_velocity;
-        }
-        else {return vert_velocity_prev;}
-    }
+        Serial.print("Temperature = ");
+        Serial.print(bmp.readTemperature());
+        Serial.println(" *C");
 
-    void readSensor() {
-        // Serial.print("Temperature = ");
-        // Serial.print(bmp.readTemperature());
-        // Serial.println(" *C");
-
-        // Serial.print("Pressure = ");
-        // Serial.print(bmp.readPressure()/100); //displaying the Pressure in hPa, you can change the unit
-        // Serial.println(" hPa");
+        Serial.print("Pressure = ");
+        Serial.print(bmp.readPressure()/100); //displaying the Pressure in hPa, you can change the unit
+        Serial.println(" hPa");
 
         Serial.print("Approx altitude = ");
-        Serial.print(bmp.readAltitude(sea_level_read));
+        Serial.print(bmp.readAltitude(sea_level_sample));
         Serial.println(" m");
         Serial.println();
     }
--- barometrs/src/main.cpp
+++ barometrs/src/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <SPI.h>
-#include <LoRa.h>
-#include <heltec.h>
 #include <WiFi.h>
 #include "barometer_wrapper.h"
 
@@ -13,27 +11,6 @@
 //define oled object
 U8X8_SSD1306_128X64_NONAME_SW_I2C oled(15, 4, 16);
 
-
-//definitions for LoRa
-#define SS 18
-#define RST 14
-#define DI0 26
-
-int syncWord = 0xF3;
-byte _localAddress = 0xFF; // address of this device
-byte _destination = 0xFF;  // destination to send to
-
-
-//*definitions for wifi
-const char *ssid = "Tele2-8c311";
-const char *password = "8g6ihj47rb2";
-
-// Set web server port number to 80
-WiFiServer server(80);
-
-// Variable to store the HTTP request
-String header;
-
 // Current time
 unsigned long currentTime = millis();
 // Previous time
@@ -42,17 +19,6 @@ unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 //*wifi end
 
-//lora message function (this can be read by the current LoPy implementation)
-void sendMessage(String outgoing, int lora_message_id)
-{
-	LoRa.beginPacket();			   // start packet
-	LoRa.write(_destination);	   // add destination address
-	LoRa.write(_localAddress);	   // add sender address
-	LoRa.write(lora_message_id);   // add message ID
-	LoRa.write(outgoing.length()); // add payload length
-	LoRa.print(outgoing);		   // add payload
-	LoRa.endPacket();			   // finish packet and send it
-}
 
 void setup()
 {
@@ -66,24 +32,6 @@ void setup()
 	oled.begin();
 	oled.setFont(u8x8_font_chroma48medium8_r);
 	oled.drawString(1, 3, "Shalom");
-
-	//Lora
-	SPI.begin(5, 19, 27, 18);
-	LoRa.setPins(SS, RST, DI0);
-	if (!LoRa.begin(433E6))
-	{
-		Serial.println("Starting LoRa failed!");
-		while (1);
-	}
-
-	//setting paramaters
-	LoRa.setSyncWord(0xF3);
-	LoRa.setTxPower(14);
-	LoRa.setSpreadingFactor(10);
-	LoRa.setCodingRate4(6);
-	LoRa.setSignalBandwidth(62.5E3);
-
-	Serial.println("Lora initialized!");
 }
 
 void loop()
