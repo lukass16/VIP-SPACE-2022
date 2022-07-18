83ae22ce4273058d7745a3b7cbfa6480ed34e139
lukass16 <lukasskellijs@gmail.com>
2022-07-18 12:28:47 +0300

    update LoRa messages

--- VIP ROCKET MAIN/include/communication.h
+++ VIP ROCKET MAIN/include/communication.h
@@ -45,7 +45,7 @@ namespace comms
         sens_data::BarometerData bar = s_data.getBarometerData();
         sens_data::BatteryData bat = s_data.getBatteryData();
         int r_state = s_data.getRocketState();
-        sprintf(outgoing, "%7.4f,%7.4f,%2d,%5.2f,%5.2f,%5.2f,%5.0f,%6.1f,%6.1f,%3.0f,%2.1f,%1d,%4d", gps.lat, gps.lng, gps.sats, imu.acc_x, imu.acc_y, imu.acc_z, bar.pressure, bar.altitude, bar.f_altitude, bar.f_velocity, bat.bat1, r_state, counter); //*imu sends acceleration in place of magnetic field strength
+        sprintf(outgoing, "%7.4f,%7.4f,%5.0f,%2d,%4.2f,%4.2f,%4.2f,%5.0f,%6.1f,%6.1f,%3.0f,%2.1f,%1d,%4d", gps.lat, gps.lng, gps.alt, gps.sats, imu.acc_x, imu.acc_y, imu.acc_z, bar.pressure, bar.altitude, bar.f_altitude, bar.f_velocity, bat.bat1, r_state, counter); //*imu sends acceleration in place of magnetic field strength
         counter++;
         return outgoing;
     }
