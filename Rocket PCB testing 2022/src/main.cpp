d36a032c561f36b410d80640c75ee516e85bac8e
lukass16 <lukasskellijs@gmail.com>
2022-10-06 22:21:24 +0300

    Update arming wrapper

--- Rocket PCB testing 2022/src/main.cpp
+++ Rocket PCB testing 2022/src/main.cpp
@@ -27,7 +27,7 @@ void setup()
 	buzzer::signalTestStart();
 	buzzer::transitionToGeneratorMode();
 
-	buzzer::signalTransition();
+	// buzzer::signalTransition();
 	// // 2) LoRa
 	// Serial.println("\nTesting LoRa");
 	// lora::setup();
@@ -39,8 +39,8 @@ void setup()
 	// 	delay(400);
 	// }
 
-	//buzzer::signalTransition();
-	// 3) Barometer
+	// buzzer::signalTransition();
+	// // 3) Barometer
 	// Wire.begin(21, 22); // initialize correct i2c lines
 	// Serial.println("\nTesting Barometer");
 	// barometer::setup();
@@ -53,58 +53,58 @@ void setup()
 	// 	delay(400);
 	// }
 
-	// buzzer::signalTransition();
-	// // 4) IMU
-	// Serial.println("\nTesting IMU");
-	// imu::setup();
-	// start_time = millis();
-	// while (millis() - start_time < interval)
-	// {
-	// 	imu::readSensor();
-	// 	imu::printAll();
-	// 	delay(400);
-	// }
+	buzzer::signalTransition();
+	// 4) IMU
+	Serial.println("\nTesting IMU");
+	imu::setup();
+	start_time = millis();
+	while (millis() - start_time < interval)
+	{
+		imu::readSensor();
+		imu::printAll();
+		delay(400);
+	}
 
-	// buzzer::signalTransition();
-	// // 5) SD
-	// Serial.println("\nTesting SD");
-	// SDcard::setup();
-	// SD_File file = SDcard::openFile();
-	// start_time = millis();
-	// while (millis() - start_time < interval)
-	// {
-	// 	SDcard::test(file);
-	// 	delay(400);
-	// }
-	// SDcard::closeFile(file);
+	buzzer::signalTransition();
+	// 5) SD
+	Serial.println("\nTesting SD");
+	SDcard::setup();
+	SD_File file = SDcard::openFile();
+	start_time = millis();
+	while (millis() - start_time < interval)
+	{
+		SDcard::test(file);
+		delay(400);
+	}
+	SDcard::closeFile(file);
 
-	// buzzer::signalTransition();
-	// // 6) EEPROM clear pin
-	// Serial.println("\nTesting EEPROM clear pin");
-	// pinMode(EEPROMCLEAR, INPUT);
-	// start_time = millis();
-	// while (millis() - start_time < interval)
-	// {
-	// 	Serial.println("EEPROM clear Pin reads: " + String(analogRead(EEPROMCLEAR)));
-	// 	delay(400);
-	// }
+	buzzer::signalTransition();
+	// 6) EEPROM clear pin
+	Serial.println("\nTesting EEPROM clear pin");
+	pinMode(EEPROMCLEAR, INPUT);
+	start_time = millis();
+	while (millis() - start_time < interval)
+	{
+		Serial.println("EEPROM clear Pin reads: " + String(analogRead(EEPROMCLEAR)));
+		delay(400);
+	}
 
 	buzzer::signalTransition();
 	// 7) Switch 1
 	Serial.println("\nTesting Switch 1");
 	pinMode(SW1, INPUT);
 	start_time = millis();
-	while (true) //!changed from millis() - start_time < interval
+	while (millis() - start_time < interval)
 	{
 		reading = analogRead(SW1);
 		Serial.println("Switch 1 reads: " + String(reading));
 
-		if(reading > threshold)
-		{
-			buzzer::buzzDigital();
-			delay(100);
-			buzzer::buzzEndDigital();
-		}
+		// if(reading > threshold)
+		// {
+		// 	buzzer::buzzDigital();
+		// 	delay(100);
+		// 	buzzer::buzzEndDigital();
+		// }
 
 		// for(int i = 0; i <= reading / 10; i++)
 		// {
@@ -114,55 +114,55 @@ void setup()
 		// 	delay(100);
 		// }
 		
-		delay(2000);
+		delay(400);
 	}
 
-	// buzzer::signalTransition();
-	// // 8) Switch 2
-	// Serial.println("\nTesting Switch 2");
-	// pinMode(SW2, INPUT);
-	// start_time = millis();
-	// while (millis() - start_time < interval)
-	// {
-	// 	Serial.println("Switch 2 reads: " + String(analogRead(SW2)));
-	// 	delay(400);
-	// }
+	buzzer::signalTransition();
+	// 8) Switch 2
+	Serial.println("\nTesting Switch 2");
+	pinMode(SW2, INPUT);
+	start_time = millis();
+	while (millis() - start_time < interval)
+	{
+		Serial.println("Switch 2 reads: " + String(analogRead(SW2)));
+		delay(400);
+	}
 
-	// buzzer::signalTransition();
-	// // 9) Pyro sense pin
-	// Serial.println("\nTesting Pyro sense pin");
-	// pinMode(PYROSENSE, INPUT);
-	// start_time = millis();
-	// while (millis() - start_time < interval)
-	// {
-	// 	Serial.println("Pyro sense pin reads: " + String(analogRead(PYROSENSE)));
-	// 	delay(400);
-	// }
+	buzzer::signalTransition();
+	// 9) Pyro sense pin
+	Serial.println("\nTesting Pyro sense pin");
+	pinMode(PYROSENSE, INPUT);
+	start_time = millis();
+	while (millis() - start_time < interval)
+	{
+		Serial.println("Pyro sense pin reads: " + String(analogRead(PYROSENSE)));
+		delay(400);
+	}
 
-	// buzzer::signalTransition();
-	// // 10) Fire pyro pin
-	// Serial.println("\nTesting Fire pyro pin");
-	// pinMode(FIREPYRO, OUTPUT);
-	// start_time = millis();
-	// while (millis() - start_time < interval)
-	// {
-	// 	digitalWrite(FIREPYRO, HIGH);
-	// 	Serial.println("Fire pyro pin set to: HIGH");
-	// 	delay(1000);
-	// 	digitalWrite(FIREPYRO, LOW);
-	// 	Serial.println("Fire pyro pin set to: LOW");
-	// 	delay(1000);
-	// }
+	buzzer::signalTransition();
+	// 10) Fire pyro pin
+	Serial.println("\nTesting Fire pyro pin");
+	pinMode(FIREPYRO, OUTPUT);
+	start_time = millis();
+	while (millis() - start_time < interval)
+	{
+		digitalWrite(FIREPYRO, HIGH);
+		Serial.println("Fire pyro pin set to: HIGH");
+		delay(1000);
+		digitalWrite(FIREPYRO, LOW);
+		Serial.println("Fire pyro pin set to: LOW");
+		delay(1000);
+	}
 
-	// buzzer::signalTransition();
-	// // 11) GPS
-	// Serial.println("\nTesting GPS");
-	// gps::setup();
-	// while(true)
-	// {
-	// 	gps::readGps();
-	// 	delay(400);
-	// }
+	buzzer::signalTransition();
+	// 11) GPS
+	Serial.println("\nTesting GPS");
+	gps::setup();
+	while(true)
+	{
+		gps::readGps();
+		delay(400);
+	}
 }
 
 void loop() {}
